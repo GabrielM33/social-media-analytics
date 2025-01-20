b696a9cb7a5e30817552f830ae1ccb8c11fbacef
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-20 16:06:09 -0300

    nextjs init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    // display counter from youtube api
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">
+        Welcome to Social Media API Project
+      </h1>
+    </div>
+  );
+}
