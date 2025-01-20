2465716b74c91087af1558e1022fb6e43b7f6964
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-20 18:04:32 -0300

    fix import error on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import YoutubeViews from "@/components/youtibe/YoutubeViews";
+import YoutubeData from "@/components/YoutubeData";
+import InputBar from "@/components/InputBar";
 
 export default function Home() {
   return (
     // display counter from youtube api
     <div className="flex flex-col items-center justify-center py-10">
       <h1 className="text-4xl font-bold">Social Media API Project</h1>
-      <YoutubeViews videoId="Z9xzPDRrQHw" />
-      <YoutubeViews videoId="dQw4w9WgXcQ" />
+      <div className="p-10">
+        <InputBar />
+      </div>
+      <YoutubeData />
     </div>
   );
 }
