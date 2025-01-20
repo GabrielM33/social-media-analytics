c6b3f9e7e7249d8d1a1ae8fc80200941e89955f4
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-20 17:30:37 -0300

    update landing page with youtube views component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import YoutubeViews from "@/components/youtibe/YoutubeViews";
+
 export default function Home() {
   return (
     // display counter from youtube api
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">
-        Welcome to Social Media API Project
-      </h1>
+    <div className="flex flex-col items-center justify-center py-10">
+      <h1 className="text-4xl font-bold">Social Media API Project</h1>
+      <YoutubeViews videoId="Z9xzPDRrQHw" />
+      <YoutubeViews videoId="dQw4w9WgXcQ" />
     </div>
   );
 }
