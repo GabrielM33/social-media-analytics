0e735e5fe6a8774918e9f5d201b6b4918218205a
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-23 12:10:27 -0300

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-// This is the server component (app/page.tsx)
-import { Suspense } from "react";
-import HomeClient from "@/components/HomeClient";
-
+import InputBarYoututbe from "@/components/InputBarYoututbe";
+import YoutubeData from "@/components/YoutubeData";
+import AggregateData from "@/components/AggregateData";
+import InputBarInstagram from "@/components/InputBarInstagram";
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HomeClient />
-    </Suspense>
+    <div className="flex flex-col items-center justify-center py-10">
+      <h1 className="text-4xl font-bold text-center">
+        Social Media API Project
+      </h1>
+      <div className="p-10 w-full max-w-4xl">
+        <InputBarInstagram />
+        {/* YouTube Section */}
+        <section>
+          <h2 className="text-2xl font-semibold mb-4">YouTube Analytics</h2>
+          <InputBarYoututbe />
+          <YoutubeData />
+        </section>
+      </div>
+
+      <div className="mt-12">
+        <AggregateData />
+      </div>
+    </div>
   );
 }
